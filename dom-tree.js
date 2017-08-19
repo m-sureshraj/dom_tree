61025eb8e33767a72310d55785df3adb7f882719
Sureshraj <m.s.suresh100@gmail.com>
2017-08-19 15:57:16 +0530

    implement move_up & move_down keyboard navigation

--- dom-tree.js
+++ dom-tree.js
@@ -3,7 +3,7 @@
 // helper fn's
 var DomTreeUtil = (function() {
 	/**
-	 * check key is a valid primitive type
+	 * check param is a valid primitive type
 	 * @param { Any } key - js type eg. string | number | etc..
 	 * @return { Boolean }
 	 */
@@ -11,9 +11,7 @@ var DomTreeUtil = (function() {
 		if (key === null)
 			return true;
 
-		const type = typeof key;
-
-		switch (type) {
+		switch (typeof key) {
 			case 'string':
 			case 'number':
 			case 'boolean':
@@ -202,8 +200,6 @@ var DomTree = (function(dom, util) {
 			}
 
 			// insert the toggleOptionNode into `element` as a first child
-			// we can easily do with ele.prepend() method, but older browsers not support
-			// for this method
 			element.insertBefore(_getToggleOptionNode(), element.firstChild);
 
 			var parentNode = dom.createElement('ul');
@@ -242,6 +238,145 @@ var DomTree = (function(dom, util) {
 		closeBlock.innerHTML = util.isArray(param) ? ']' : '}';
 
 		return closeBlock;
+	}
+
+	function _moveDown(ele) {
+		ele.classList.remove('dtjs-highlight');
+
+		// we focused next element because of move down
+		if (
+			ele.classList.contains('hc') &&
+			(!ele.classList.contains('dtjs-mu') || ele.parentNode.classList.contains('dtjs-root'))
+		) {
+			var firstChild = ele.querySelector('ul').firstChild;
+
+			if (firstChild && firstChild.classList.contains('hc')) {
+				firstChild.className += ' dtjs-md';
+			}
+		} else {
+			ele.nextSibling &&
+			ele.nextSibling.classList.contains('hc') &&
+			ele.nextSibling.classList.add('dtjs-md');
+		}
+
+		if (ele.classList.contains('hc')) {
+			if (ele.classList.contains('dtjs-mu') && ele.nextSibling) {
+				// has next sibling so highlight it
+				ele.classList.remove('dtjs-mu');
+				ele.nextSibling.className += ' dtjs-highlight';
+			} else if (ele.classList.contains('dtjs-mu')) {
+				// no next sibling so going up again
+				ele.classList.remove('dtjs-mu');
+
+				if (ele.parentNode.classList.contains('dtjs-root')) {
+					// reached to the root top then highlight it's first element
+					ele.querySelector('ul').firstChild.className += ' dtjs-highlight';
+				} else {
+					ele.offsetParent.className += ' dtjs-highlight dtjs-mu';
+				}
+			} else {
+				// highlight first child
+				ele.classList.remove('dtjs-md');
+				ele.querySelector('ul').firstChild.className += ' dtjs-highlight';
+			}
+		} else {
+			// next ele
+			var nextSibling = ele.nextSibling;
+
+			if (nextSibling) {
+				nextSibling.className += ' dtjs-highlight';
+			} else {
+				ele.offsetParent.className += ' dtjs-highlight dtjs-mu';
+			}
+		}
+	}
+
+	function _moveUp(ele) {
+		ele.classList.remove('dtjs-highlight');
+
+		// we focused next element because of move up
+		if (
+			ele.classList.contains('hc') &&
+            (!ele.classList.contains('dtjs-md') || ele.parentNode.classList.contains('dtjs-root'))
+		) {
+			var lastChild = ele.querySelector('ul').lastChild;
+
+			if (lastChild && lastChild.classList.contains('hc')) {
+			    console.log('bingo');
+				lastChild.className += ' dtjs-mu';
+			}
+		} else {
+			if (ele.previousSibling && ele.previousSibling.classList.contains('hc')) {
+				ele.previousSibling.classList.add('dtjs-mu');
+			}
+		}
+
+		if (ele.classList.contains('hc')) {
+			if (ele.classList.contains('dtjs-md') && ele.previousSibling) {
+				// has previous sibling so highlight it
+				ele.classList.remove('dtjs-md');
+				ele.previousSibling.className += ' dtjs-highlight';
+			} else if (ele.classList.contains('dtjs-md')) {
+				ele.classList.remove('dtjs-md');
+
+				if (ele.parentNode.classList.contains('dtjs-root')) {
+					ele.querySelector('ul').lastChild.className += ' dtjs-highlight';
+				} else {
+					ele.offsetParent.className += ' dtjs-highlight dtjs-md';
+				}
+			} else {
+				ele.classList.remove('dtjs-mu');
+				ele.querySelector('ul').lastChild.className += ' dtjs-highlight';
+			}
+		} else {
+			// prev ele
+			var prevSibling = ele.previousSibling;
+
+			if (prevSibling) {
+				prevSibling.className += ' dtjs-highlight';
+			} else {
+				ele.offsetParent.className += ' dtjs-highlight dtjs-md';
+			}
+		}
+	}
+
+	function _collapse(ele) {
+		if (ele.classList.contains('hc') && ele.classList.contains('fold')) {
+			ele.classList.remove('fold');
+		}
+	}
+
+	function _fold(ele) {
+		if (ele.classList.contains('hc') && !ele.classList.contains('fold')) {
+			ele.classList.add('fold');
+		}
+	}
+
+	function _handleKeyboardNavigation(tree, keyCode) {
+		var highlightedEle = tree.querySelector('.dtjs-highlight');
+
+		// if there are no highlighted element yet then highlight tree's first child
+		if (!highlightedEle && (keyCode === 40 || keyCode === 38)) {
+			tree.firstChild.className += ' dtjs-highlight';
+			return;
+		}
+
+		switch (keyCode) {
+			case 40:
+				_moveDown(highlightedEle);
+				break;
+			case 38:
+				_moveUp(highlightedEle);
+				break;
+			case 39:
+                _collapse(highlightedEle);
+				break;
+			case 37:
+				_fold(highlightedEle);
+				break;
+			default:
+				return;
+		}
 	}
 
 	function _createInstancePropWithDefaultConfig(userConfig, defaultConfig, instance) {
@@ -291,7 +426,7 @@ var DomTree = (function(dom, util) {
 
   		if (!_isValidConfigData(configDataPropType)) {
 			throw Error('config.data property should be a object '
-				+ 'or Array or Json but received ' + configDataPropType);
+				+ 'or Array or JSON but received ' + configDataPropType);
   		}
 
   		// if config.data type is string then, it should be a valid json
@@ -328,12 +463,31 @@ var DomTree = (function(dom, util) {
 		constuctor: DomTree,
 		init: function() {
 			var tree = _constructDomTree(this.data, null);
-			tree.className = 'domTreeJsRoot';
+			tree.className = 'dtjs-root';
+			tree.setAttribute('tabindex', 0);
 
 			// add theme option class
 			if (this.theme) {
 				tree.className += (' ' + this.theme);
 			}
+
+			tree.addEventListener('focus', function() {
+				util.handleToggleClass(tree, 'dtjs-root-focused');
+			});
+
+			tree.addEventListener('blur', function() {
+				util.handleToggleClass(tree, 'dtjs-root-focused');
+			});
+
+			tree.addEventListener('keydown', function(e) {
+				var keyCode = e.keyCode;
+				// when `.dtjs-root` is focused prevent horizontal, vertical
+				// up and down scroll event
+				if (keyCode >= 37 && keyCode <= 40) {
+					e.preventDefault();
+					_handleKeyboardNavigation(tree, keyCode);
+				}
+			});
 
 			// register click event listener on toggleOption node via eventDelegation
 			tree.addEventListener('click', function(e) {
@@ -368,15 +522,21 @@ var DomTree = (function(dom, util) {
 // tree collapse status should be configurable (done)
 // add open, close bracket class names for styling purpose (done)
 // enable theme option via configuration (done)
-// implement full key board navigation feature (working)
+// what if user passed a function as a property (done)
 
 // todo
+// implement full key board navigation feature (working)
+// _createInstancePropWithDefaultConfig - optimize
 // refactor createEntry fn (must)
 // highlight matching bracket
 // Array.prototype.indexOf does not work in IE9 need polyfill
+// check querySelector() supoort in IE9
 
 // later
 // create separate file for helpers fn
 // browser target IE9 and above
 
 // http://stackoverflow.com/questions/3656467/is-it-possible-to-focus-on-a-div-using-javascript-focus-function
+
+// nova color scheme
+// https://trevordmiller.com/projects/nova
